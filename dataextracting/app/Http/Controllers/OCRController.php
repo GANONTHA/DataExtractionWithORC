e76db0408de0b6b2deea6e8476bcdb9d6919ea0c
Bruno <bganontha@gmail.com>
2024-05-23 17:29:23 +0100

    implement pdf file  processing with tesseract

--- dataextracting/app/Http/Controllers/OCRController.php
+++ dataextracting/app/Http/Controllers/OCRController.php
@@ -35,4 +35,35 @@ class OCRController extends Controller
         //return the OCR output
         return response()->json(['ocr_output' => $ocr_output], 200);
     }
+
+    //process pdf
+    public function processPdf(Request $request)
+    {
+        //validate the pdf
+        $request->validate([
+            'pdf' => 'required|mimes:pdf'
+        ]);
+
+        //get pdfPath file and store it in the folder uploaded_pdf
+        $pdfPath = $request->file('pdf')->store('uploaded_pdf');
+
+        //output path
+        $outputPath = 'ocr_output.txt';
+
+        //execute the tesseract command
+        $command = "tesseract $pdfPath $outputPath";
+        $result = shell_exec($command);
+
+        //Read the OCR output from the file
+        $ocr_output = file_get_contents($outputPath);
+
+        //handle error if the command fails
+        if (!$result) {
+            // $ocr_output = "Error: Command failed";
+            return response()->json(['error' => 'OCR processing failed.'], 500);
+        }
+
+        //return the OCR output
+        return response()->json(['ocr_output' => $ocr_output], 200);
+    }
 }
